7b840747760792fc93bc6e36e8102e93f8fd778b
sivagnani <merugagnani@gmail.com>
2023-01-10 11:22:44 +0530

    10/01/2023

---
+++ Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    public class Appliance
+    {
+        public int applianceId;
+        public string name;
+        public Appliance(string name,int id)
+        {
+            this.name = name;
+            this.applianceId = id;
+        }
+    }
+    public class Fan : Appliance{
+        public Fan(string name,int n) : base(name,n){ }
+    }
+    public class AC : Appliance {
+        public AC(string name,int n) : base(name,n) { }
+    }
+    public class Bulb : Appliance {
+        public Bulb(string name,int n) : base(name,n) { }
+    }
+    public class Switch
+    {
+        public int switchId;
+        public bool state;
+        public Switch(int id)
+        {
+            this.switchId = id;
+            this.state = false;
+        }
+
+    }
+    public class Operations
+    {
+        public int getIntInput()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public List<Appliance> CreateDevices(int fan, int ac, int bulb)
+        {
+            List<Appliance> Device = new List<Appliance>();
+            int id = 1;
+            for (int i = 1; i <= fan; i++)
+            {
+                Device.Add(new Fan("Fan" + " " + (i.ToString()),id));
+                id = id + 1;
+            }
+            for (int i = 1; i <= ac; i++)
+            {
+                Device.Add(new AC("AC" + " " + (i.ToString()),id));
+                id = id + 1;
+            }
+            for (int i = 1; i <= bulb; i++)
+            {
+                Device.Add(new Bulb("Bulb" + " " + (i.ToString()),id));
+                id = id + 1;
+            }
+            return Device;
+        }
+        public List<Switch> CreateSwitches(int sum)
+        {
+            List<Switch> switches = new List<Switch>();
+            for(int i = 1; i <= sum; i++)
+            {
+                switches.Add(new Switch(i));
+            }
+            return switches;
+        }
+        public bool getStatebyId(List<Switch> s,int id)
+        {
+            foreach (Switch sw in s)
+            {
+                if(sw.switchId == id)
+                {
+                    return (sw.state);
+                }
+            }
+            return false;
+        }
+        public string getStateString(bool s) {
+            return s ? "On" : "Off";
+        }
+        public string getNamebyId(List<Appliance> device,int id) {
+            foreach(Appliance a in device)
+            {
+                if (a.applianceId == id)
+                {
+                    return a.name;
+                }
+            }
+            return "Not found";
+        }
+        public void changeState(List<Switch> s,int id)
+        {
+            foreach (Switch sw in s)
+            {
+                if (sw.switchId == id)
+                {
+                    sw.state = !sw.state;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,80 +1,68 @@
 ﻿using System;
-
+using System.Collections;
+using Home;
 public class Program
 {
-    static Dictionary<string,string> appliances = new Dictionary<string, string>();
-    static string[] arr;
     public static void Main(string[] args)
     {
+        Operations obj = new Operations();
         Console.Write("Enter the number of Fans: ");
-        int fan = Convert.ToInt32(Console.ReadLine());
+        int fan = obj.getIntInput();
         Console.Write("Enter the number of ACs: ");
-        int ac = Convert.ToInt32(Console.ReadLine());
+        int ac = obj.getIntInput();
         Console.Write("Enter the number of Bulbs: ");
-        int bulb = Convert.ToInt32(Console.ReadLine());
+        int bulb = obj.getIntInput();
         int sum = fan + ac + bulb;
-        arr = new string[sum];
-        Create("Fan",fan);
-        Create("AC",ac);
-        Create("Bulb",bulb);
-        int i = 0;
-        foreach(KeyValuePair<string,string> kv in appliances)
+        List<Appliance> devices = new List<Appliance>();
+        List<Switch> SwitchBoard = new List<Switch>();
+        devices = obj.CreateDevices(fan,ac,bulb);
+        SwitchBoard = obj.CreateSwitches(sum);
+        string Oppstate;
+        string name;
+        int i = 1;
+        while (true)
         {
-            arr[i] = kv.Key;
-            i++;
-        }
-        Menu();
-    }
-    public static void Menu()
-    {
-        Console.WriteLine("MENU :");
-        for(int i = 0; i < arr.Length;i++)
-        {
-            Console.WriteLine((i+1).ToString()+". "+arr[i]+" is \"" + appliances[arr[i]]+"\"");
-        }
-        Console.WriteLine((arr.Length+1).ToString() + ". " +"Exit");
-        Console.Write("Select one option from the menu ");
-        int op = Convert.ToInt32(Console.ReadLine());
-        if(op <= arr.Length)
-        {
-            Edit(op);
-        }
-        else if (op == (arr.Length + 1))
-        {
-            Console.WriteLine("Exiting............");
-        }
-        else
-        {
-            Console.WriteLine("Please Enter valid Option");
-        }        
-    }
-    public static void Edit(int op)
-    {
-        string state = (appliances[arr[op - 1]] == "Off" ? "On" : "Off");
-        Console.WriteLine("1. " + "Switch " + arr[op - 1] +" "+ state);
-        Console.WriteLine("2. " + "Back");
-        Console.Write("Select One option from Above: ");
-        int option = Convert.ToInt32(Console.ReadLine());
-        if(option == 1) 
-        {
-            appliances[arr[op - 1]] = state;
-            Menu();
-        }
-        else if(option == 2)
-        {
-            Menu();
-        }
-        else
-        {
-            Console.WriteLine("Please Enter valid Option");
-        }
-    }
-    public static void Create(string device, int count)
-    {
-        for(int i=1; i<=count; i++)
-        {
-            string dev = device+" "+(i.ToString());
-            appliances.Add(dev, "Off");
+            Console.WriteLine("MENU :");
+            foreach(var a in devices)
+            {
+                string id = (a.applianceId).ToString();
+                bool deviceState = obj.getStatebyId(SwitchBoard, a.applianceId);
+                Console.WriteLine(id + ". " + a.name + " is \"" + obj.getStateString(deviceState) + "\"");
+            }
+            Console.WriteLine((sum+1).ToString() + ". " + "Exit");
+            Console.Write("Select one option from the menu ");
+            int op = obj.getIntInput();
+            if (op <= sum)
+            {
+                name = obj.getNamebyId(devices,op);
+                Oppstate = obj.getStateString(!obj.getStatebyId(SwitchBoard,op));
+                Console.WriteLine("1. " + "Switch " + name + " " + Oppstate);
+                Console.WriteLine("2. " + "Back");
+                Console.Write("Select One option from Above: ");
+                int option = obj.getIntInput();
+                if (option == 1)
+                {
+                    obj.changeState(SwitchBoard,op);
+                }
+                else if (option == 2)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter valid Option");
+                }
+
+            }
+            else if (op == (sum + 1))
+            {
+                Console.WriteLine("Exiting............");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please Enter valid Option");
+            }
         }
     }
 }
